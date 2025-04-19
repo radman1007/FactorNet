7731be284b1d927f6f833b0c28a7219dffac2a93
radman1007 <radman.cod110@gmail.com>
2025-04-20 00:46:47 +0330

    add features page

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('features/', views.features, name='features'),
 ]
--- home/views.py
+++ home/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def features(request):
+    return render(request, 'features.html')
