2b5a3b70b73d10cc6cd1ab142777a5bfb25e076e
radman1007 <radman.cod110@gmail.com>
2025-04-20 01:47:58 +0330

    add a dashboard page

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,10 +5,9 @@ from .models import UserProfile
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        fields = ['full_name', 'email', 'address', 'logo']
+        fields = ['full_name', 'email', 'address']
         widgets = {
             'full_name': forms.TextInput(attrs={'class': 'form-control'}),
             'email': forms.EmailInput(attrs={'class': 'form-control'}),
             'address': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
-            'logo': forms.ClearableFileInput(attrs={'class': 'form-control'}),
         }
