543d5dce008841d90165492ae435c41bd53aa1d8
radman1007 <radman.cod110@gmail.com>
2025-04-28 21:48:19 +0330

    change and hide panel admin url

--- config/urls.py
+++ config/urls.py
@@ -18,9 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .data import DJANGO_ADMIN_URL
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(DJANGO_ADMIN_URL, admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('', include('home.urls')),
     path('invoice/', include('invoices.urls')),
