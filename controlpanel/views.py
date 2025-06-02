f1adc2cb20aa5fbe5268fd67ced4df3c52d90d62
radman1007 <radman.cod110@gmail.com>
2025-06-02 16:19:02 +0330

    add login required and resolver match for control panel

--- controlpanel/views.py
+++ controlpanel/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def index_control_panel(request):
     return render(request, 'index_control_panel.html')
