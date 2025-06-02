4764da1dcce22b7438100b408de8f5df47cc3592
radman1007 <radman.cod110@gmail.com>
2025-06-02 23:42:12 +0330

    add invoices page in control panel

--- controlpanel/urls.py
+++ controlpanel/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index_control_panel, name='index_control_panel'),
     path('charts/', views.chart_control_panel, name='chart_control_panel'),
+    path('invoices/', views.invoices_control_panel, name='invoices_control_panel'),
 ]
--- controlpanel/views.py
+++ controlpanel/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from invoices.models import Invoice
 
 @login_required
 def index_control_panel(request):
@@ -12,3 +13,13 @@ def chart_control_panel(request):
     if not request.user.is_staff:
         return redirect('index')
     return render(request, 'chart_control_panel.html')
+
+@login_required
+def invoices_control_panel(request):
+    if not request.user.is_staff:
+        return redirect('index')
+    invoices = Invoice.objects.all()
+    context = {
+        'invoices' : invoices
+    }
+    return render(request, 'invoices_control_panel.html', context)
