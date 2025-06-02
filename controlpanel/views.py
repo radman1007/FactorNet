bce3529dd8b671f98fc7d92419348352bc4b1fe7
radman1007 <radman.cod110@gmail.com>
2025-06-02 23:16:29 +0330

    now just staff user can see control panel

--- controlpanel/views.py
+++ controlpanel/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from accounts.models import User
-from django.db.models import Q
 
 @login_required
 def index_control_panel(request):
+    if not request.user.is_staff:
+        return redirect('index')
     return render(request, 'index_control_panel.html')
 
 @login_required
 def chart_control_panel(request):
+    if not request.user.is_staff:
+        return redirect('index')
     return render(request, 'chart_control_panel.html')
