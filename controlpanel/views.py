9858a418d1e77c21a55d800e5f85b7cbbbcf83d4
radman1007 <radman.cod110@gmail.com>
2025-06-02 16:56:23 +0330

    show all users in control panel

--- controlpanel/views.py
+++ controlpanel/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from accounts.models import User
 
 @login_required
 def index_control_panel(request):
-    return render(request, 'index_control_panel.html')
+    users = User.objects.all()
+    context = {
+        'users' : users,
+    }
+    return render(request, 'index_control_panel.html', context)
