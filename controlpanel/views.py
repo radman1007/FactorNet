57f7a3fd80258eac3020e3c37dee67838cb603d6
radman1007 <radman.cod110@gmail.com>
2025-06-02 17:34:13 +0330

    now can search in user in control panel

--- controlpanel/views.py
+++ controlpanel/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from accounts.models import User
+from django.db.models import Q
 
 @login_required
 def index_control_panel(request):
     users = User.objects.all()
+    search_user = request.GET.get('search_user', '').strip()
+    if search_user:
+        users = User.objects.filter(
+            Q(phone__icontains=search_user)|Q(profiles__full_name__icontains=search_user)
+        )
     context = {
         'users' : users,
+        'search_user' : search_user,
     }
     return render(request, 'index_control_panel.html', context)
