e7c5c955be679735fcf3339f410bededb7e0bb37
radman1007 <radman.cod110@gmail.com>
2025-06-04 21:33:03 +0330

    now can save all contact message

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Contact
 
 def index(request):
     return render(request, 'index.html')
@@ -7,4 +8,14 @@ def features(request):
     return render(request, 'features.html')
 
 def help(request):
+    if request.method == 'POST':
+        full_name = request.POST.get('full_name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        Contact.objects.create(
+            full_name=full_name,
+            email=email,
+            message=message
+        )
+        return redirect('index')
     return render(request, 'help.html')
