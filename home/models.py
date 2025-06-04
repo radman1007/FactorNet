fa31f62e69d38b907940886c2bf9fdb0c516f24d
radman1007 <radman.cod110@gmail.com>
2025-06-04 21:23:49 +0330

    add contact models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    full_name = models.CharField(max_length=255, verbose_name='نام کامل')
+    email = models.EmailField(verbose_name='ایمیل')
+    message = models.TextField(verbose_name='پیام')
+    
+    def __str__(self):
+        return f"{self.full_name : {self.message}}"
